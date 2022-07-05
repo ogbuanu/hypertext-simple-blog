affba12deb1c770b2111d23add763b1f95c53188
Ikenna Ogbuanu John <ogbuanuikenna66@gmail.com>
2022-07-05 17:15:55 +0100

    new change

--- api/index.js
+++ api/index.js
@@ -3,15 +3,12 @@ const {homeRoute} = require("../routes/home")
 
 const app = express()
 
-<<<<<<< HEAD
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(homeRoute)
 
-=======
 app.get("/", (req, res) => {
     res.send("done!")
 })
->>>>>>> dda04b1fd7e97b32a3543c529f311077c4a255b6
 
 app.listen(3000)
